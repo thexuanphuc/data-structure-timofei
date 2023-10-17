f7135ddcf85390fcc02acdaa889aae91f0a3e2c5
thexuanphuc <phucrussia10@gmail.com>
2023-10-17 17:04:06 +0300

    lecture 21 fall

---
+++ lec21/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "mylibrary.h"
+#include "mylibrary.h"
+#include "mylibrary.h"
+#include "mylibrary.h"
+
+
+int main()
+{
+    std::cout<<" enter 2 numbers:  ";
+    int A, B, n;
+    std::cin >> A >> B;
+    std::cout << karatsuba(A,B) << '\n' ;
+    return 0;
+}
---
+++ lec21/mylibrary.h
@@ -0,0 +1,6 @@
+#ifndef THE_my_library_GUARD
+#define THE_my_library_GUARD 
+static int count = 0;
+int get_num(int x);
+int karatsuba(int A, int B);
+#endif // THE_my_library_GUARD
